8940b9ac723c4a0ea8c40adecd5da88e74aa4138
relivnd <hej+github@tobiasvones.ch>
2023-03-03 09:46:02 +0100

    mod configured gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,15 +28,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `EASIT-Funnel`,
+        short_name: `EASIT`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#DD356E`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Easit-Icon.png`, // This path is relative to the root of the site.
+        legacy: false,
+        theme_color_in_head: false, // This will avoid adding theme-color meta tag.
+        // Place where crossOrigin attribute can be added.
       },
     },
   ],
