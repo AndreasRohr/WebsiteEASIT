e8909b7aa182f0884cce6cd0608d7dedfcc600b5
Tobias Vonesch <tobias@teqly.ch>
2023-03-16 07:28:45 +0100

    add rudimental table

--- src/sections/lifecycle.js
+++ src/sections/lifecycle.js
@@ -1,9 +1,69 @@
 import * as React from "react";
 import SectionContainer from "../components/sectionContainer";
+import styled from "@emotion/styled"
+import {StaticImage} from "gatsby-plugin-image";
+
+const Bulletpoint = styled.h2`
+  
+`
+
+const Table = styled.table`
+  width: 100%;
+`
+
+const Td = styled.td`
+  height: 10em;
+  text-align: center;
+`
 
 const Lifecycle = () => (
   <SectionContainer color={"#9ED8DB"}>
     <h1>Lifecycle</h1>
+      <Table>
+        <tr>
+            <Td>
+                <Bulletpoint>Auswählen</Bulletpoint>
+            </Td>
+            <Td>
+                <StaticImage
+                    src="../images/auswaehlen.png"
+                    loading="eager"
+                    width={50}
+                    formats={["auto", "webp", "avif"]}
+                    alt="Checkmark"
+                />
+            </Td>
+        </tr>
+        <tr>
+            <Td>
+                <Bulletpoint>Bestellen</Bulletpoint>
+            </Td>
+            <Td>
+                <StaticImage
+                    src="../images/bestellen.png"
+                    loading="eager"
+                    width={50}
+                    formats={["auto", "webp", "avif"]}
+                    alt="Checkmark"
+                />
+            </Td>
+        </tr>
+          <tr>
+                <Td>
+                    <Bulletpoint>Loslegen</Bulletpoint>
+                </Td>
+                <Td>
+                    <StaticImage
+                        src="../images/loslegen.png"
+                        loading="eager"
+                        width={50}
+                        formats={["auto", "webp", "avif"]}
+                        alt="Checkmark"
+                    />
+                </Td>
+            </tr>
+
+      </Table>
   </SectionContainer>
 )
 
