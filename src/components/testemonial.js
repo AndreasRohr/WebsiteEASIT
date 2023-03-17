9fa4318911b3f1b34b4a2b879a6e4d1b56b77ede
Tobias Vonesch <tobias@teqly.ch>
2023-03-17 12:25:05 +0100

    add testimonials section without styling

--- src/components/testemonial.js
+++ src/components/testemonial.js
@@ -1,15 +1,39 @@
 import * as React from "react"
 import styled from "@emotion/styled"
+//import GatsbyImage from "gatsby-plugin-image";
 
-const Container = styled.div`
-  background: #dd356e;
-  width: 100%;
+const Card = styled.div`
+  background: #ffffff;
+  color: #000000;
+  width: 30vw;
+  text-align: center;
 `
 
-const Testimonial = () => (
-    <Container>
+// export const query = graphql`
+//   query ($logo: String) {
+//     file(relativePath: { eq: $logo }) {
+//       childImageSharp {
+//         gatsbyImageData(
+//         width: 100
+//         placeholder: BLURRED
+//         formats: [AUTO, WEBP, AVIF]
+//         )
+//       }
+//     }
+//   }
+// `
 
-    </Container>
-)
+const Testimonial = ({name, content}) => {
+
+//    const image = useStaticQuery(query, {logo: logo});
+
+    return (
+        <Card>
+            <h2>{name}</h2>
+            <p>{content}</p>
+            {/*<GatsbyImage image={image}/>*/}
+        </Card>
+    )
+}
 
 export default Testimonial
