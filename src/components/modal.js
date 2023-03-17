d135c02534abf6ed9a0254a23808c2796316ed5b
Tobias Vonesch <tobias@teqly.ch>
2023-03-17 15:16:25 +0100

    add basic modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,20 +1,37 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-import Pill from "../components/pill";
 
-const ModalContainer = styled.div`
+const ModalPage = styled.div`
+    position: fixed;
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+`
 
+const ModalContent = styled.div`
+  position: fixed;
+  text-align: center;
+  margin: auto;
+  background-color: white;
+  width: 50%;
+  top: 30vh;
+  left: 25%;
 `
 
 const Modal = ({toggleModal}) => {
 
     return (
         <>
-            <ModalContainer>
-                <h1>Modal</h1>
-                <p>Modal content</p>
-                <Pill action={toggleModal}>Close</Pill>
-            </ModalContainer>
+            <ModalPage>
+                <ModalContent>
+                    <a onClick={toggleModal}>Close</a>
+                </ModalContent>
+            </ModalPage>
         </>
     )
 }
