23b66aa887893e1cbdb64e918c5be4b6f898c616
Jannik Frei <jannik.frei@students.fhnw.ch>
2023-03-23 20:43:32 +0100

    FAQ Cards

--- src/components/collapsible.js
+++ src/components/collapsible.js
@@ -1,31 +1,29 @@
 import * as React from "react"
-import styled from "@emotion/styled"
-
-const Container = styled.div`
-    padding: 1em;
-`
-
-const Button = styled.button`
-
-`
-
-const Content = styled.p`
-  
-`
+import "../styles/collapsible.css"
 
 const Collapsible = ({title, content}) => {
+    const [isActive, setIsActive] = React.useState({card: false, heading: false});
+    const [isContentVisible, setIsContentVisible] = React.useState(false);
 
-    const [open, setOpen] = React.useState(false);
-
-    const toggle = () => {
-        open ? setOpen(false) : setOpen(true);
+    const toggleCollapsible = () => {
+        setIsActive({card: !isActive.card, heading: !isActive.heading});
+        setIsContentVisible(!isContentVisible);
     }
 
     return (
-        <Container>
-            <Button onClick={toggle}>{title}</Button>
-            { open && <Content>{content}</Content> }
-        </Container>
+        <div className={`toggle-card ${isActive.card ? 'active' : ''}`}>
+            <div className={`toggle-heading ${isActive.heading ? 'active' : ''}`} onClick={toggleCollapsible}>
+                <h4 className="toggle-heading-text">{title}</h4>
+                <button className={`toggle-card-icon ${isActive.card ? 'active' : ''}`}>
+                    <svg id="Regular" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path className="cls-1" d="M23.25,7.311,12.53,18.03a.749.749,0,0,1-1.06,0L.75,7.311"/>
+                    </svg>
+                </button>
+            </div>
+            <div className={`toggle-content ${isContentVisible ? 'show' : ''}`}>
+                <p>{content}</p>
+            </div>
+        </div>
     )
 }
 
--- src/sections/faq.js
+++ src/sections/faq.js
@@ -2,10 +2,16 @@ import * as React from "react"
 import SectionContainer from "../components/sectionContainer";
 import Collapsible from "../components/collapsible";
 import data from "../content/faq.json"
+import styled from "@emotion/styled"
+
+const Title = styled.h1`
+text-align: center;
+margin-bottom: 3rem;
+`
 
 const Faq = () => (
     <SectionContainer color={"#ffffff"}>
-        <h1>FAQ</h1>
+        <Title>Die wichtigsten Fragen</Title>
         {data.questions.map((item, index) => {
             return <><Collapsible key={index} title={item.title} content={item.content} /> <br /></>
         })}
