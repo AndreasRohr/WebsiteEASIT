f0d0e0797fe754b92cefd18dd58db6fc0a708171
Tobias Vonesch <tobias@teqly.ch>
2023-03-30 16:54:34 +0200

    add Alle Fragen Button

--- src/sections/importantFaq.js
+++ src/sections/importantFaq.js
@@ -3,6 +3,8 @@ import SectionContainer from "../components/sectionContainer";
 import Collapsible from "../components/collapsible";
 import data from "../content/faq.json"
 import styled from "@emotion/styled"
+import Pill from "../components/pill"
+import { navigate } from "gatsby"
 
 const Title = styled.h1`
   text-align: center;
@@ -25,6 +27,7 @@ const ImportantFaq = () => {
         {impFAQ.map((item, index) => {
             return <><Collapsible key={index} title={item.title} content={item.content} /> <br /></>
         })}
+        <Pill action={() => navigate("/faq")}>Alle Fragen</Pill>
         </FaqWrapper>
     </SectionContainer>
 )}
