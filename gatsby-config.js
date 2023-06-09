d2b9071fed4d83a9dee969a843192b01287b11b1
Tobias Vonesch <tobias@teqly.ch>
2023-06-09 15:37:06 +0200

    add basic working gtag setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,24 +64,21 @@ module.exports = {
       {
           resolve: `gatsby-plugin-google-gtag`,
           options: {
-              trackingIds: [
-                  "G-2FHN7FDZC3" // Ihre Google Analytics-Tracking-ID hier einfügen
-              ],
-              pluginConfig: {
-                  head: false,
-                  respectDNT: true,
-                  exclude: ["/preview/**", "/do-not-track/me/too/"],
-                  // Falls Sie die Option 'origin' verwenden möchten, setzen Sie diese auf die gewünschte URL
-              },
+            // You can add multiple tracking ids and a pageview event will be fired for all of them.
+            trackingIds: [
+              "G-2FHN7FDZC3", // Google Analytics / GA
+            ],
+            // This object gets passed directly to the gtag config command
+            // This config will be shared across all trackingIds
+            gtagConfig: {
+              anonymize_ip: true,
+            },
+            // This object is used for configuration specific to this plugin
+            pluginConfig: {
+              // Puts tracking script in the head instead of the body
+              head: true,
+            },
           }
-      },
-      {
-          resolve: `gatsby-plugin-google-tagmanager`,
-          options: {
-            id: "GTM-TPDGPRJ",
-            includeInDevelopment: false,
-            enableWebVitalsTracking: true
-          },
       },
   ],
 }
